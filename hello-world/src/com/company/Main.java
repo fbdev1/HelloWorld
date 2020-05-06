e24905e3ffbeb8c00c2abf2f169b915bf27e69ec
Syrkin Victor <syrkinvictor@gmail.com>
2020-05-06 15:08:59 +0500

    First commit

--- hello-world/src/com/company/Main.java
+++ hello-world/src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    String hello = "Hello";
-    String world = "World!";
+        String hello = "Hello";
+        String world = "World!";
         System.out.println(hello + " " + world);
-     }
+    }
 }
